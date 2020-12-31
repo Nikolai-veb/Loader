7f93898095c107e025ab6f9929955d40949b9610
Nikloay <nik.vagin1995@mail.ru>
2020-12-31 21:59:37 +0300

    Create new test_views

--- gallery/tests/test_views.py
+++ gallery/tests/test_views.py
@@ -10,7 +10,7 @@ class ImageListViewTest(TestCase):
         # Create 13 authors for pagination tests
         number_of_image = 13
         for image_num in range(number_of_image):
-            Images.objects.create(title=f'Image {image_num}')
+            Images.objects.create(title=f'Image {image_num}', url='https://wallpapertag.com/wallpaper/full/b/1/a/376474-gorgerous-hd-background-images-2560x1600-xiaomi.jpg' )
 
     def test_view_url_exists_at_desired_location(self):
         resp = self.client.get('')
@@ -28,4 +28,19 @@ class ImageListViewTest(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'gallery/image_detail.html')
 
+    def test_view_uses_correct_template_create_image(self):
+        resp = self.client.post(reverse('created_image'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'gallery/create_image.html')
 
+    def test_view_uses_correct_template_resizing(self):
+        image = Images.objects.get(id=1)
+        resp = self.client.get(reverse('resizing_image', kwargs={'slug': image.slug}))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'gallery/resizing_image.html')
+
+    # def test_view_uses_correct_template_handler_image(self):
+    #     image = Images.objects.get(id=1)
+    #     resp = self.client.post(reverse('resizing_handler', kwargs={'pk': image.id}))
+    #     self.assertEqual(resp.status_code, 200)
+    #     self.assertTemplateUsed(resp, 'gallery/image_detail.html')
